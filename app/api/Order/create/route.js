346c49bbc2727fe977ae26e2750482109fef8a15
vijaydevguy <vijayakumarjob01@gmail.com>
2025-08-31 14:53:37 +0530

    order place option

---
+++ app/api/Order/create/route.js
@@ -0,0 +1,51 @@
+import { inngest } from "@/config/inngest";
+import Product from "@/models/Product";
+import User from "@/models/User";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    const { userId } = getAuth();
+
+    const { address, items } = await req.json();
+
+    if (!address || items.length === 0) {
+      return NextResponse.json({ success: false, message: "Invalid data" });
+    }
+
+    //calculate amount using items
+    //reason why we are used reduce here bcz i wont change existing array as well it will return accumulated answer value
+    const amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+
+      return acc + product.offerPrice * item.quantity;
+    }, 0);
+
+    await inngest.send({
+      name: "order/created",
+      data: {
+        userId,
+        address,
+        items,
+        amount,
+        amount: amount + Math.floor(amount * 0.02),
+        date: Date.now(),
+      },
+    })
+
+
+    //clear user cart
+    const user = await User.findById(userId)
+
+    user.cartItems = {}
+
+    await user.save()
+
+    return NextResponse.json({success:true, message:'Order Placed'})
+
+  } catch (error) {
+
+    return NextResponse.json({success:false,message: error.message})
+  }
+}
--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDb from "./db";
 import User from "@/models/User";
+import Order from "@/models/Order";
 
 //this inngest.js is working like a controller
 
@@ -70,5 +71,37 @@ export const syncUserDeletion = inngest.createFunction(
 
     await connectDb();
     await User.findByIdAndDelete(id);
+  }
+);
+
+//Inngest function to create user's in database
+
+export const createUserOrder = inngest.createFunction(
+  {
+    id: "create-user-order",
+    batchEvents: {
+      maxSize: 5,
+      timeout: "5s",
+    },
+  },
+  {
+    event: "order/created",
+  },
+
+  async ({ events }) => {
+    const orders = events.map((event) => {
+      return {
+        userId: event.data.userId,
+        items: event.data.items,
+        amount: event.data.amount,
+        address: event.data.address,
+        date: event.data.date,
+      };
+    });
+
+    await connectDb();
+    await Order.insertMany(orders);
+
+    return { success: true, processed: orders.length };
   }
 );
